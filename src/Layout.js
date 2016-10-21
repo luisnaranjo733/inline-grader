51a912397039875cb6009d540379e97e91616cd5
Luis Naranjo <luisnaranjo733@gmail.com>
2016-10-21 12:30:27 -0700

    Add react-router and history npm modules
    Also include layout.js from previous commit, which was forgotten.

---
+++ src/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from './Home';
+import CriteriaGrader from './CriteriaGrader';
+
+class Layout extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+export default Layout;
