3bd5fcbf1204d0a1e6159ea3238d9ab65ed8adc9
Luis Naranjo <luisnaranjo733@gmail.com>
2016-10-21 00:02:25 -0700

    add basic text input form for rubric url

--- src/App.js
+++ src/App.js
@@ -1,20 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import {Button} from  'react-bootstrap';
+import {Jumbotron, Button, Form, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 import './App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <Jumbotron className="hidden-xs">
+        <h1>Inline grader</h1>
+        <p>Reference the rubric. Grade your students. Leave them comments. All in one place!</p>
+        <p><Button bsStyle="primary">Learn more</Button></p>
+      </Jumbotron>
+    );
+  }
+}
+
+class UploadRubricForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getValidationState() {
+    const length = this.state.value.length;
+    if (length > 0) return 'success';
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+  
+  handleSubmit(e) {
+    console.log("Hello");
+  }
+
+  render() {
+    var formGroupStyle = {
+      margin: "1em"
+    };
+    var formControlStyle = {
+      marginTop: "1em",
+      marginBottom: "1em"
+    };
+
+    return (
+      <Form>
+        <FormGroup
+          controlId="rubricLinkInput"
+          validationState={this.getValidationState()}
+          style={formGroupStyle}
+        >
+          <ControlLabel>Submit rubric URL</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Enter rubric URL"
+            onChange={this.handleChange}
+            style={formControlStyle}
+          />
+          <FormControl.Feedback />
+          <button onClick={this.handleSubmit} className="btn btn-default">Submit</button>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button>Click me!</Button>
+        <Header />
+        <UploadRubricForm />
       </div>
     );
   }
