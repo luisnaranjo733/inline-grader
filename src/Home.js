135d78011052985f4034f65763d3fe45106ee5c8
Luis Naranjo <luisnaranjo733@gmail.com>
2016-10-22 00:31:13 -0700

    add xml parser
    update prototype xml documents
    add proof of concept xml parsing code (incomplete)

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,56 @@ import React, { Component } from 'react';
 import {Link} from 'react-router';
 import {Jumbotron, Button, Form, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 
+var parse = require('xml-parser');
+
+function validateXML(parsedXML) {
+  console.log(parsedXML);
+  /*
+	• <rubric> rules
+		○ Root tag must be a <rubric> tag.
+		○ <rubric> tag must have a "name" attribute.
+	• <section> rules
+		○ All <section> tags must have a "name" attribute
+		○ All <section> tags must have 1 or more child tags
+		○ All <section> tags must satisfy one of the following rules
+			§ All direct children are <criteria> tags
+			§ All direct children are <section> tags
+		
+	• <criteria> rules
+		○ All <criteria> tags must have "name" attribute.
+		○ All <criteria> tags must have "weight" attribute.
+  */
+  var root = parsedXML['root']; // root tag of xml document
+
+  
+  if (root['name'] === 'rubric') { 
+    var rubricName = root['attributes']['name'];
+    if (rubricName && rubricName.length > 0) {
+      
+      var topLevelSection = root['children'];
+      topLevelSection.forEach(function(section) {
+        console.log(section);
+      });
+
+
+    } else {
+
+    }
+  } else {
+    // invalid xml, root tag should be of type <rubric>
+  }
+
+}
+
+var url = "https://raw.githubusercontent.com/luisnaranjo733/inline-grader/master/prototype-markup/accessibility-rubric.xml?token=ABCn22fxxKiAbfNrlrm5Y7wdisW1bGcPks5YFE6vwA%3D%3D";
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+        var parsedXML = parse(xmlHttp.responseText);
+        validateXML(parsedXML);
+}
+xmlHttp.open("GET", url, true); // true for asynchronous 
+xmlHttp.send(null);
 
 class Header extends Component {
   render() {
@@ -20,7 +70,7 @@ class UploadRubricForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: ''
+      url: 'https://raw.githubusercontent.com/luisnaranjo733/inline-grader/master/prototype-markup/accessibility-rubric-notescaped.xml?token=ABCn23FAtD39Lfb7Lj3OgOxn8tN2rs9bks5YE7eQwA%3D%3D'
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -36,14 +86,7 @@ class UploadRubricForm extends Component {
   }
   
   handleSubmit(e) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-            console.log(xmlHttp.responseText);
-            console.log(xmlHttp.responseXML);
-    }
-    xmlHttp.open("GET", this.state.url, true); // true for asynchronous 
-    xmlHttp.send(null);
+
   }
 
   render() {
