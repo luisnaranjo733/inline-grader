ad41f576ef8ad3ec5f75f9b458d41312ab9c8028
Luis Naranjo <luisnaranjo733@gmail.com>
2016-10-23 15:49:39 -0700

    Refactor and comment code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import CriteriaGrader from './CriteriaGrader';
-import {Home} from './Home';
-import { Router, Route, hashHistory } from 'react-router';
+import Rubric from './Rubric';
+
+import { Router, Route, hashHistory, Link } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import {Jumbotron, Button, Form, FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
+
+class Header extends Component {
+  render() {
+    return (
+      <Jumbotron className="hidden-xs">
+        <h1>Inline grader</h1>
+        <p>Reference the rubric. Grade your students. Leave them comments. All in one place!</p>
+        <p><Button bsStyle="primary">Learn more</Button></p>
+      </Jumbotron>
+    );
+  }
+}
+class UploadRubricForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://raw.githubusercontent.com/luisnaranjo733/inline-grader/master/prototype-markup/accessibility-rubric.xml?token=ABCn26exuHdJkqOLGCO52H_0pPQCVXGeks5YFOoawA%3D%3D'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ url: e.target.value });
+  }
+  
+  handleSubmit(e) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+          var rubric = new Rubric(xmlHttp.responseText);
+          console.log(rubric.name);
+          console.log(rubric.criteria);
+        }
+            
+    }
+    xmlHttp.open("GET", this.state.url, true); // true for asynchronous 
+    xmlHttp.send(null);
+  }
+
+  render() {
+    var formGroupStyle = {
+      margin: "1em"
+    };
+    var formControlStyle = {
+      marginTop: "1em",
+      marginBottom: "1em"
+    };
+
+    return (
+      <Form>
+        <FormGroup
+          controlId="rubricLinkInput"
+          style={formGroupStyle}
+        >
+          <ControlLabel>Submit rubric URL</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.url}
+            placeholder="Enter rubric URL"
+            onChange={this.handleChange}
+            style={formControlStyle}
+          />
+          <FormControl.Feedback />
+          <button type="button" onClick={this.handleSubmit} className="btn btn-default">Submit</button>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <Header />
+        <UploadRubricForm />
+        <Link to="criteriaGrader">Criteria</Link>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <Router history={hashHistory}>
