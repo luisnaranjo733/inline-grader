8924327978cd23c0b73fa9a8c9f5a99977fc33c0
Luis Naranjo <luisnaranjo733@gmail.com>
2016-10-24 09:26:43 -0700

    Re-add app.js (decided not to have everything in index.js)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Home} from './Index';
+import {Home} from './App';
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Home />, div);
+});
+
+it('sums', () => {
+  var x = 1+1;
 });
