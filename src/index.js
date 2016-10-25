2be853766bd8612351789f615b6c24b146daa958
Luis Naranjo <luisnaranjo733@gmail.com>
2016-10-25 11:59:26 -0700

    Switch routing style to hashHistory for github pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './index.css'
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
 import Navigation from './Navigation'
 import HomePage from './modules/HomePage'
 import CriteriaPage from './modules/CriteriaPage'
 
 render((
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={Navigation}>
 
       {/* add it here, as a child of `/` */}
