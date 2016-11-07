fe70482cecd027bbc5bb7030642784a6052b873b
Luis Naranjo <luisnaranjo733@gmail.com>
2016-11-07 11:10:04 -0800

    Add fixed nav bar (mostly for dev)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import { Link } from 'react-router'
 
+class FixedNavBar extends Component {
+  render() {
+
+    const navBarStyle = {
+      borderBottom: 'thin solid black',
+      backgroundColor: 'white'
+    }
+
+    return (
+      <nav style={navBarStyle}>
+        <ul>
+          <li><Link to="/">{'Inline Grader'}</Link></li>
+          <li><Link to="/criteria/1">criteria 1</Link></li>
+          <li><Link to="/report">Report</Link></li>
+        </ul>
+      </nav>
+
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>navbar will go here</p>
-        <Link to="/">Home</Link>
-        <Link to="/criteria/1">criteria 1</Link>
-        <Link to="/report">Report</Link>
+        <FixedNavBar />
         {this.props.children}
       </div>
     );
