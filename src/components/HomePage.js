71b6f06124da9006c71713ae1d4008a69a92070b
Luis Naranjo <luisnaranjo733@gmail.com>
2016-11-09 10:36:17 -0800

    Update rubric xml url

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -43,7 +43,7 @@ class HomePage extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      rubricUrl: 'https://raw.githubusercontent.com/info343-au16/grading/xml/memory-rubric.xml?token=ABCn25OYnFEa9YTSCrYjEzSqz7AlrBITks5YLI_KwA%3D%3D'
+      rubricUrl: 'https://raw.githubusercontent.com/info343-au16/grading/xml/memory-rubric.xml?token=ABCn26VOJU_ExZjhseoSXDfA2mlYn9Z2ks5YLKb9wA%3D%3D'
     };
 
     this.onUrlChanged = this.onUrlChanged.bind(this);
