0ef7f8268e048412b0b1bff1d3054f3af8126771
Luis Naranjo <luisnaranjo733@gmail.com>
2016-11-17 17:02:09 -0800

    Basic render unit tests for report and criteria pages passing

--- src/components/CriteriaPage.test.js
+++ src/components/CriteriaPage.test.js
@@ -5,19 +5,18 @@ import Criterion from '../models/Criterion';
 import {isXmlValid, parseXml} from '../helpers/parseRubric';
 import ConnectedCriteriaPage, {CriteriaPage} from './CriteriaPage';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  var params = {
+var redux_and_router_params = {};
+
+beforeAll(function() {
+  redux_and_router_params['params'] = {
       criteriaIndex: '1'
   };
 
   let fileContents = readFileSync('src/test-fixtures/pass/includes_all_features.xml', 'utf8');
-  var rubric = parseXml(fileContents);
+  Object.assign(redux_and_router_params, parseXml(fileContents));
+});
 
-  ReactDOM.render(
-    <CriteriaPage
-      params={params}
-      rubricName={rubric.rubricName}
-      criteria={rubric.criteria}
-    />, div);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<CriteriaPage {...redux_and_router_params} />, div);
 });
--- src/components/ReportPage.test.js
+++ src/components/ReportPage.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {readFileSync} from 'fs';
+import Criterion from '../models/Criterion';
+import {isXmlValid, parseXml} from '../helpers/parseRubric';
 import ConnectedReportPage, {ReportPage} from './ReportPage';
+
+var redux_and_router_params = {};
+
+beforeAll(function() {
+  redux_and_router_params['params'] = {
+      criteriaIndex: '1'
+  };
+
+  let fileContents = readFileSync('src/test-fixtures/pass/includes_all_features.xml', 'utf8');
+  Object.assign(redux_and_router_params, parseXml(fileContents));
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-//   ReactDOM.render(<ReportPage />, div);
+  ReactDOM.render(<ReportPage {...redux_and_router_params} />, div);
 });
